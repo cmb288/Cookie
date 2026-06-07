3bbc2e80afe851b446372c0402b52bf6596e0c7e
cmb288 <natalie.befeld@web.de>
2026-06-07 21:54:12 +0200

    cookie_game.py

--- cookie_game.py
+++ cookie_game.py
@@ -4,8 +4,8 @@ import math
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Cookie Game")
 clock = pygame.time.Clock()
